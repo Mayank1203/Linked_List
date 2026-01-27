5f3bbc1ed2829e7ea6c5a05362291022e3faa3b0
Mayank1203 <mayankpatle1203@gmail.com>
2026-01-27 22:37:33 +0530

    tenth day Second Commit

---
+++ src/HasCycle.java
@@ -0,0 +1,41 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+public class HasCycle {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(1);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node1;
+
+        System.out.println(hasCycle(node1));
+
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+
+        return false;
+    }
+}
+
+// Question Link -> https://leetcode.com/problems/linked-list-cycle/
